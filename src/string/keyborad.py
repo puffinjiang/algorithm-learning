bdb751411701e21b05aedb307fe50b3f95058734
jiangpf <36837780+jiangpf-michael@users.noreply.github.com>
2021-11-02 21:55:59 +0800

    Add keyword

--- src/string/keyborad.py
+++ src/string/keyborad.py
@@ -11,3 +11,23 @@
 @Software   : PyCharm
 
 """
+from typing import List
+
+
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        keyword_set1 = {'w', 'r', 'p', 't', 'u', 'y', 'e', 'o', 'q', 'i'}
+        keyword_set2 = {'s', 'k', 'g', 'h', 'd', 'l', 'j', 'a', 'f'}
+        keyword_set3 = {'n', 'z', 'v', 'm', 'c', 'x', 'b'}
+        res = []
+        for word in words:
+            word_set = set(list(word.lower()))
+            if word and (not word_set.difference(keyword_set1) or not word_set.difference(
+                    keyword_set2) or not word_set.difference(keyword_set3)):
+                res.append(word)
+        return res
+
+
+if __name__ == '__main__':
+    words = ["Hello", "Alaska", "Dad", "Peace"]
+    print(Solution().findWords(words))
