c9f5d0386c02ddceca9674bdf0b981d2b70332aa
jiangpf <36837780+jiangpf-michael@users.noreply.github.com>
2021-11-21 17:06:00 +0800

    feat(node) Add deep max

---
+++ src/node/deep_max.py
@@ -0,0 +1,21 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        depth = 1
+        for node in root.children:
+            depth = max(self.maxDepth(node) +1 ,1)
+        return depth
