79d51abbf67403f6376e1d8cbfcdc25d41f1ed63
puffinjiang <36837780+puffinjiang@users.noreply.github.com>
2022-09-24 17:35:46 +0800

    feat(sort): Add bubble sort and quick sort

---
+++ tests/sort/test_qiuck_sort.py
@@ -0,0 +1,18 @@
+#! python3
+# -*- encoding: utf-8 -*-
+'''
+@File    :   test_qiuck_sort.py
+@Time    :   2022/09/24 12:08:11
+@Author  :   puffin jiang
+@Version :   1.0
+'''
+
+
+import random
+
+from tests.sort.quick_sort import quick_sort
+
+
+def test_quick_sort():
+    arr = random.sample(range(1, 20), 8)
+    assert sorted(arr) == quick_sort(arr, 0, len(arr)-1)
